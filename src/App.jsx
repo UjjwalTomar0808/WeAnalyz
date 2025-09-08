23ff7ee76467680de2ec8777ac46eea83bb8482a
Saurabh Kumar Kashinwar <skashinwar@gmail.com>
2025-09-08 14:16:30 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Navbar from './Components/Navbar';
-// import Home from './Components/Home';
+
 import Signin from './Authentication/Signin';
 import Signup from './Authentication/Signup';
-import Forgotpassword from './Authentication/Forgotpassword';
-import Dashboard from './Components/Dashboard.jsx';
-import NewTicket from './Components/NewTicket.jsx';
-import MyTicket from './Components/MyTicket.jsx';
+import ForgotPassword from './Authentication/Forgotpassword';
+import Dashboard from './Components/Dashboard';
+import NewTicket from './Components/NewTicket';
+import MyTicket from './Components/MyTicket';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
         <Routes>
+          <Route path="/" element={<Signin />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/forget-password" element={<Forgotpassword />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/new_ticket" element={<NewTicket />} />
-          <Route path="/my_ticket" element={<MyTicket />} />
-          <Route path="/" element={<Signin />} />
+          <Route path="/new-ticket" element={<NewTicket />} />
+          <Route path="/my-ticket" element={<MyTicket />} />
+          
+          {/* Fallback for undefined routes */}
+          <Route path="*" element={<Navigate to="/signin" replace />} />
         </Routes>
+      </Router>
     </div>
   );
 }
